66ef94cb7597e93df55d05e513ab3ff35479dffc
BAN1337 <joaocleber.jc10@gmail.com>
2024-06-14 16:30:11 -0300

    Atualização de códigos - Aula 340

--- config/database.js
+++ config/database.js
@@ -4,4 +4,4 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/javascriptNote', { 
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('Connection Succesfull'))
-.catch((err) => console.error(err))  
+.catch((err) => console.error(err))
