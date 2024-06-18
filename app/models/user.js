a54e8e63139b59a30b61a25fe1b6c9fe3416d9e2
BAN1337 <joaocleber.jc10@gmail.com>
2024-06-17 23:07:53 -0300

    Atualização de códigos - Aula 346

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 let userSchema = new mongoose.Schema({
     name: String,
@@ -7,5 +8,31 @@ let userSchema = new mongoose.Schema({
     created_at: {type: Date, default: Date.now},
     updated_at: {type: Date, default: Date.now}
 })
+
+userSchema.pre('save', function(next) {
+    if(this.isNew || this.isModified('password')){
+        bcrypt.hash(this.password, 10, 
+            (err, hashedPassword) => {
+                if(err)
+                    next(err)
+                else {
+                    this.password = hashedPassword
+                    next()
+                }
+            }
+        )
+    }
+})
+
+userSchema.methods.isCorrectPassword = function(password, next){
+    bcrypt.compare(password, this.password, 
+        function(err, same){
+            if(err)
+                next(err)
+            else
+                next(err, same)
+        }
+    )
+}
 
 module.exports = mongoose.model('User', userSchema)
