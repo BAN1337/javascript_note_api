5b5ae2626e6d1b437090d3c116e5a283e3061068
BAN1337 <joaocleber.jc10@gmail.com>
2024-07-17 15:46:15 -0300

    Atualização de códigos - Aula 370

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -3,91 +3,91 @@ const router = express.Router();
 const Note = require("../models/note")
 const withAuth = require('../middlewares/auth')
 
-router.post('/', withAuth, async(req, res) => {
-    const {title, body} = req.body
-    
+router.post('/', withAuth, async (req, res) => {
+    const { title, body } = req.body
+
     try {
-        let note = new Note({title: title, body: body, author: req.user._id})
+        let note = new Note({ title: title, body: body, author: req.user._id })
         await note.save()
         res.status(200).json(note)
     } catch (error) {
-        res.status(500).json({error: 'Problem to create a new note'})
+        res.status(500).json({ error: 'Problem to create a new note' })
     }
 })
 
-router.get('/search', withAuth, async(req, res) => {
-    const {query} = req.query
+router.get('/search', withAuth, async (req, res) => {
+    const { query } = req.query
     try {
-        let notes = await Note.find({author: req.user._id}).find({$text: {$search: query}})
-        
+        let notes = await Note.find({ author: req.user._id }).find({ $text: { $search: query } })
+
         res.json(notes)
     } catch (error) {
-        res.status(500).json({error:error})
+        res.status(500).json({ error: error })
     }
 })
 
-router.get('/:id', withAuth, async(req, res) => {
+router.get('/:id', withAuth, async (req, res) => {
     try {
-        const {id} = req.params
+        const { id } = req.params
         let note = await Note.findById(id)
-        if(isOwner(req.user, note))
+        if (isOwner(req.user, note))
             res.json(note)
         else
-            res.status(403).json({error: 'Permission denied'})
+            res.status(403).json({ error: 'Permission denied' })
 
     } catch (error) {
-        res.status(500).json({error: 'Problem to get a note'})
+        res.status(500).json({ error: 'Problem to get a note' })
     }
 })
 
-router.put('/:id', withAuth, async(req, res) => {
+router.put('/:id', withAuth, async (req, res) => {
     const { title, body } = req.body
     const { id } = req.params
 
     try {
         let note = await Note.findById(id)
-        if(isOwner(req.user, note)){
-            let note = await Note.findByIdAndUpdate(id, 
-                {$set: {title: title, body: body}},
-                {upsert: true, 'new': true}
+        if (isOwner(req.user, note)) {
+            let note = await Note.findByIdAndUpdate(id,
+                { $set: { title: title, body: body } },
+                { upsert: true, 'new': true }
             )
 
             res.json(note)
-        }else{
-            res.status(403).json({error: 'Permission denied'})
+        } else {
+            res.status(403).json({ error: 'Permission denied' })
         }
     } catch (error) {
-        res.status(500).json({error: 'Problem to update a note'})
+        res.status(500).json({ error: 'Problem to update a note' })
     }
 })
 
-router.delete('/:id', withAuth, async(req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
     const { id } = req.params
 
     try {
         let note = await Note.findById(id)
-        if(isOwner(req.user, note)){
+        if (isOwner(req.user, note)) {
             await note.deleteOne()
-            res.json({message: `OK to delete note ${id}`})
-        }else{
-            res.status(403).json({error: 'Permission denied'})
+            res.json({ message: `OK to delete note ${id}` })
+        } else {
+            res.status(403).json({ error: 'Permission denied' })
         }
     } catch (error) {
-        res.status(500).json({error: 'Problem to delete a note'})
+        res.status(500).json({ error: 'Problem to delete a note' })
     }
 })
 
-router.get('/', async(req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
-        let notes = await Note.find()
+        let notes = await Note.find({ author: req.user._id })
         res.send(notes)
     } catch (error) {
-        res.status(500).json({error: 'Problem to get a notes'})
+        res.status(500).json({ error: 'Problem to get a notes' })
     }
 })
 
 const isOwner = (user, note) => {
-    if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
+    if (JSON.stringify(user._id) == JSON.stringify(note.author._id))
         return true
     else
         return false
--- app/routes/users.js
+++ app/routes/users.js
@@ -5,49 +5,49 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const secret = process.env.JWT_TOKEN
 
-router.post('/register', async(req, res) => {
-  const {name, email, password} = req.body
-  const user = new User({name, email, password})
-  
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body
+  const user = new User({ name, email, password })
+
   try {
     await user.save()
     res.status(200).json(user)
   } catch (error) {
-    res.status(500).json({error: 'Error registering new user'})
+    res.status(500).json({ error: 'Error registering new user' })
   }
 })
 
-router.post('/login', async(req, res) => {
-  const {email, password} = req.body
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
 
   try {
-    let user = await User.findOne({email})
-    if(!user)
-      res.status(401).json({error: 'Incorrect email or password'})
-    else{
-      user.isCorrectPassword(password, 
-        function(err, same){
-          if(!same)
-            res.status(401).json({error: 'Incorrect email or password'})
-          else{
-            const token = jwt.sign({email}, secret, {expiresIn: '10d'})
-            res.json({user: user, token: token})
+    let user = await User.findOne({ email })
+    if (!user)
+      res.status(401).json({ error: 'Incorrect email or password' })
+    else {
+      user.isCorrectPassword(password,
+        function (err, same) {
+          if (!same)
+            res.status(401).json({ error: 'Incorrect email or password' })
+          else {
+            const token = jwt.sign({ email }, secret, { expiresIn: '10d' })
+            res.json({ user: user, token: token })
           }
         }
       )
     }
   } catch (error) {
-    res.status(500).json({error: 'Internal Error, please try again'})
+    res.status(500).json({ error: 'Internal Error, please try again' })
   }
 })
 
-/*router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   try {
-      let users = await User.find()
-      res.send(users)
+    let users = await User.find()
+    res.send(users)
   } catch (error) {
-      res.status(500).json({error: 'Problem to get a users'})
+    res.status(500).json({ error: 'Problem to get a users' })
   }
-})*/
+})
 
 module.exports = router;
